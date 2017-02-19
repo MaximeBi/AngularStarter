dc6a7c536482ae35f17ab1ca9553e4a333a94d02
MaximeBi <maxime.bigonne@gmail.com>
2017-02-19 22:10:36 +0100

    Finish the animation section (keyframes)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,14 +15,70 @@ import { Component, trigger, state, animate, style, transition, keyframes } from
       transition('closed <=> open', [
         animate("1000ms 2000ms")
       ])
-    ])
-  ],
+    ]),
+
+    trigger('animateQuote',[
+      transition('stateA <=> stateB', [
+          animate(600, keyframes([
+            style({
+              opacity: .5, offset: 0
+            }),
+            style({
+              opacity: 1, color: "#fcb514", offset: .5
+            }),
+            style({
+              opacity: .7, offset: .7
+            }),
+            style({
+              opacity: 1, offset: 1
+            })
+          ]))
+        ])
+      ])
+    ],
   selector: 'home',
-  templateUrl: 'home.component.html'
+  templateUrl: 'home.component.html',
+  styles: [
+    `
+      .frame{
+        margin-bottom: 10px;
+        padding: 10px;
+        border: 5px solid #eeeeee;
+        height: 170px;
+      }
+      .citation{
+        font-size: 20px;
+        color: #3268ba;
+      }
+    `
+  ]
 })
 export class HomeComponent{
   open:boolean = false;
   toggleElement:string = "closed" ;
+
+  animateQuote: string = "stateB";
+  activeIndex: number = 0;
+
+  quotes: quote[] = [
+    {
+      id: 0,
+      text: "aa aa aa aa",
+      author: "Toto"
+    },
+    {
+      id: 1,
+      text: "cc cc cc cc",
+      author: "Titi"
+    },
+    {
+      id: 2,
+      text: "ab ab ab ab",
+      author: "Tutu"
+    }
+  ]
+
+  quote: quote = this.quotes[0];
 
   toggle(){
     this.open = !this.open;
@@ -32,4 +88,41 @@ export class HomeComponent{
       this.toggleElement = 'closed';
     }
   }
+
+  getPreviousQuote(){
+    this.animateQuote = this.animateQuote === 'stateA' ? 'stateB' : 'stateA';
+    this.getSomeQuote(-1);
+    console.log(this.activeIndex);
+  }
+
+  getNextQuote(){
+    this.animateQuote = this.animateQuote === 'stateA' ? 'stateB' : 'stateA';
+    this.getSomeQuote(1);
+    console.log(this.activeIndex);
+  }
+
+  getSomeQuote(increment: number){
+    this.activeIndex = this.activeIndex + increment;
+
+    if(this.activeIndex >= this.quotes.length && increment === 1){
+      this.activeIndex = 0;
+    }
+
+    if(this.activeIndex < 0 && increment === -1){
+      this.activeIndex = this.quotes.length - 1;
+    }
+
+    this.quote = this.quotes[this.activeIndex];
+  }
+
+  log(event: any){
+    console.log(event);
+  }
+
+}
+
+interface quote {
+  id: number;
+  text: string;
+  author: string;
 }
